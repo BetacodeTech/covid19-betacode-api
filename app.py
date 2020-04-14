637fd9243a55d3e68c742ac145b4a21291abbc0a
Marco Mendão <mac.mendao@gmail.com>
2020-04-14 14:56:54 +0100

    changed port

--- app.py
+++ app.py
@@ -16,4 +16,4 @@ api.add_resource(Infection, Infection.ENDPOINT)
 api.add_resource(Countries, Countries.ENDPOINT)
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5000)
+    app.run(debug=True, port=5010)
